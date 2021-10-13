4429109b7bf74affacea913cd7b2e36599b79908
Luis Mario Mercado Antelo <a2019116634@estudiantes.upsa.edu.bo>
2021-10-13 16:13:12 -0400

    Modelo cancion creao

---
+++ pilincha/Models/Cancion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pilincha.Models
+{
+    public class Cancion
+    {
+        [Key]
+        [Required]
+        public string Nombre { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "nombre requerido")]
+        public string Autor { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "nombre requerido")]
+        [StringLength(200,MinimumLength =10, ErrorMessage = "La longitud es mucha")]
+        public string Letra { get; set; }
+        [StringLength(200, MinimumLength = 10, ErrorMessage = "La longitud es mucha")]
+
+        [Required]
+        public string Enlace { get; set; }
+
+    }
+}
